dcd12c62f5f4bf2d1d65af1dae3a319e0664015c
prielango <codeacademy2019@gmail.com>
2019-05-09 16:18:10 +0300

    rearchitecture

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <?php
 
 $formFields = [
-    [
-        'type' => 'text',
-        'name' => 'username',
-        'placeholder' => 'username',
-        'label' => 'Enter username'
+    'inputs' => [
+        'username' => [
+            'type' => 'text',
+            'name' => 'username',
+            'placeholder' => 'username',
+            'label' => 'Enter username'
+        ],
+        'password' => [
+            'type' => 'password',
+            'name' => 'password',
+            'placeholder' => 'password',
+            'label' => 'Enter password'
+        ],
+        'email' => [
+            'type' => 'email',
+            'name' => 'email',
+            'placeholder' => 'email',
+            'label' => 'Enter email'
+        ],
     ],
-    [
-        'type' => 'password',
-        'name' => 'password',
-        'placeholder' => 'password',
-        'label' => 'Enter password'
-    ],
-    [
-        'type' => 'email',
-        'name' => 'email',
-        'placeholder' => 'email',
-        'label' => 'Enter email'
-    ],
-    [
-        'type' => 'submit',
-        'name' => 'submit',
-        'label' => 'Submit'
-    ],
-
+    'buttons' => [
+        'submit' => [
+            'type' => 'submit',
+            'name' => 'submit',
+            'label' => 'Submit'
+        ],
+    ]
 ]
 
 ?>
@@ -40,15 +43,14 @@ $formFields = [
 
 <body>
     <form method="POST">
-        <?php foreach ($formFields as $field) : ?>
+        <?php foreach ($formFields['inputs'] as $input) : ?>
             <label>
-                <?php if ($field['type'] === 'submit') : ?>
-                    <button type="submit" name="<?php print $field['name']; ?>"><?php print $field['label']; ?></button>
-                <?php else : ?>
-                    <span><?php print $field['label']; ?></span>
-                    <input type="<?php print $field['type']; ?>" name="<?php print $field['name']; ?>" placeholder="<?php print $field['placeholder']; ?>" />
-                <?php endif; ?>
+                <span><?php print $input['label']; ?></span>
+                <input type="<?php print $input['type']; ?>" name="<?php print $input['name']; ?>" placeholder="<?php print $input['placeholder']; ?>" />
             </label>
+        <?php endforeach; ?>
+        <?php foreach ($formFields['buttons'] as $button) : ?>
+            <button type="submit" name="<?php print $button['name']; ?>"><?php print $button['label']; ?></button>
         <?php endforeach; ?>
     </form>
 </body>
